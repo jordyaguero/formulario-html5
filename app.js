349fab30de91e97d320b26d8a6c50512a81590eb
jordyaguero <jordytiradotorres@hotmail.com>
2017-08-03 10:41:29 -0500

    add property valueAsNumber

--- app.js
+++ app.js
@@ -15,6 +15,65 @@
             saveBtnClicked = true;
         };
         saveBtn.addEventListener('click', saveForm, false);
+
+        // calcular total x producto y orden total
+
+        let qtyFields = orderForm.quantyty,
+            totalFields = document.getElementsByClassName('item_total'),
+            orderTotalFields = document.getElementById('order_total');
+
+        let formatMoney = function (value) {
+            /*Devuelve un número formateado para moneda, utilizando una coma
+             como un carácter separador 1,000*/
+            return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }; // fin de formatMoney
+
+        // Calcula los totales de cada elemento y el total del pedido.
+        let calculateTotals = function () {
+            let i = 0,
+                ln = qtyFields.length,
+                itemQty = 0,
+                itemPrice = 0.00,
+                itemTotal = 0.00,
+                itemTotalMoney = '$0.00',
+                orderTotal = 0.00,
+                orderTotalMoney = '$0.00';
+
+                for (; i<ln; i++) {
+                    if (!!qtyFields[i].valueAsNumber) { // comprueba la existencia de valueAsNumber, !! Se utiliza para convertir la propiedad valueAsNumber en un tipo booleano.
+                        itemQty = qtyFields[i].valueAsNumber || 0;
+                    }
+                    else {
+                        itemQty = parseFloat(qtyFields[i].value) || 0;
+                    }
+                }// fir del for
+
+        }; // fin de calculateTotals
+
+        calculateTotals();
+
+        let qtyListeners = function () {
+            let i = 0,
+                ln = qtyFields.length;
+
+            // Llama a la función qtyListeners para agregar escuchas de eventos a los campos.
+            for ( ; i<ln; i++) {
+                qtyFields[i].addEventListener('input', calculateTotals, false);
+                qtyFields[i].addEventListener('keyup', calculateTotals, false);
+            }
+
+        };// fin de qtylisteners
+
+        qtyListeners();
+
+        /*
+         Cuando se lee la propiedad valueAsNumber de un tipo de entrada numérica, la propiedad devuelve el número
+          como un número de punto flotante. Si asigna un número de coma flotante a la propiedad valueAsNumber de un tipo
+           de entrada numérica, la propiedad convertirá el número de coma flotante en un valor basado en cadena.
+        * */
+
+        // valueAsDate es para los campos de fecha y hora
+
     };// fin de init
 
     window.addEventListener('load', init, false);
